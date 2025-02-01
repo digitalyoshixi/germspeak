4eaf057eb95313bf9b3a8dee480f20f302384df9
yoshixi <digitalyoshixi@proton.me>
2025-02-01 16:20:32 -0500

    AST classes

--- interpreter.py
+++ interpreter.py
@@ -68,6 +68,90 @@ def tokenizer(filecontents : str) -> list[(Token,str)]:
 # ------------------ Parser and AST -----------------------
 # creating the AST Tree helper functions
 
+class NodeType(Enum):
+    # Tree Types
+    Program = "Program"
+    # Statements
+    ExpressionStatement = "ExpressionStatement"
+    # Expressions
+    InfixExpression = "InfixExpression"
+    # Literals
+    IntegerLiteral = "IntegerLiteral"
+
+class Node():
+    def type(self) -> NodeType:
+        pass 
+    # pure debugging to be able to see AST in json format
+    def json(self) -> dict: 
+        pass 
+
+class Statement(Node):
+    pass
+
+class Expression(Node):
+    pass
+
+class Program(Node):
+    def __init__(self) -> None:
+        self.statements : list[Statement] = []
+
+    def type(self) -> NodeType:
+        return NodeType.Program
+    
+    def json(self) -> dict: 
+        return {
+                "type" : self.type().value,
+                # list comprehension
+                "statements" : [{stmt.type().value : stmt.json()} for stmt in self.statements]
+            }
+# Statements
+
+class ExpressionStatement(Statement):
+    def __init__(self, expr : Expression = None) -> None:
+        self.expr : Expression = expr 
+
+    def type(self) -> NodeType:
+        return NodeType.ExpressionStatement
+
+    def json(self) -> dict:
+        return {
+                "type" : self.type().value,
+                "expr" : self.expr.json()
+                }
+
+class InfixExpression(Expression):
+    def __init__(self, left_node : Expression, operator : str, right_node : Expression = None):
+        self.left_node : Expression = left_node
+        self.operator : str = operator
+        self.right_node : Expression = right_node
+
+    def type(self):
+        return NodeType.InfixExpression
+
+    def json(self) -> dict:
+        return {
+                "type" : self.type().value,
+                "left_node" : self.left_node.json(),
+                "operator" : self.operator,
+                "right_node" : self.right_node.json()
+                }
+
+class IntegerLiteral(Expression):
+    def __init__(self, value : int = None) -> None:
+        self.value : int = value
+
+    def type(self) -> NodeType:
+        return NodeType.IntegerLiteral
+
+    def json(self) -> dict:
+        return {
+                "type" : self.type().value,
+                "value" : self.value
+            }
+
+
+
+# creating the Parser Object
 class PrecedenceType(Enum):
     P_LOWEST = 0
     P_EQUALS = 1
@@ -85,13 +169,26 @@ PRECEDENCES : dict[Token, Precedences] = {
         Token.Star : PrecedenceType.P_PRODUCT,
 }
 
-class AST(object):
+class Parser(object):
     def __init__(self, tokens):
         self.tokens = tokens 
         self.token_counter = 0
+        self.current_token = tokens[0] # 
+        self.peek_token = tokens[1] # next token we peek at
         self.errors= []
-    def expect_error(self, tt : Token, got : Token):
-        self.errors.append(f"Expected {tt} and got {got} instead")
+
+    # token incrementing
+    def next_token(self) -> bool:
+        token_counter = token_counter + 1 
+        if token_counter < len(tokens) - 1:
+            self.current_token = self.tokens[token_counter]
+            self.peek_token = self.tokens[token_counter+1]
+            return True
+        else:
+            self.current_token = self.peek_token
+            return False
+
+    # operator binding power precendence
     def current_precedence(self) -> PrecedenceType:
         # get the precedence enum of the current token
         precedence = PRECEDENCES.get(self.tokens[self.token_counter][0].type)
@@ -105,6 +202,16 @@ class AST(object):
         if precedence == None:
             return PrecedenceType.P_LOWEST
         return precedence
+
+    # errors
+    def expect_error(self, tt : Token, got : Token):
+        self.errors.append(f"Expected {tt} and got {got} instead")
+    def prefix_error(self, tt : Token):
+        self.errors.append(f"No prefix function for {tt}, you are probably using the wrong operator")
+    
+    # main parsing loop 
+    def parse_program(self) -> None:
+        pass
 
 
 
